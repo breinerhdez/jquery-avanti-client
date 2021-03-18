1d260168bd7b048b9d5d981b7685a0376b4c3eaf
Breiner E. Hernández G <ing.breiner.hernandez@gmail.com>
2021-03-18 01:19:17 -0500

    First commit

---
+++ js/01-selectores.js
@@ -0,0 +1,36 @@
+/**
+ * https://jquery.com/
+ * https://jqueryvalidation.org/validate/
+ * https://getbootstrap.com/docs/3.3/getting-started/
+ * https://jqueryui.com/
+ */
+$(document).ready(function () {
+  // en esta sección se escribe el código que debe ejecutar cuando se cargue el DOM
+});
+// en esta sección se escribe el código que se dispara con eventos
+
+/**
+ * Guía
+ *
+ * 1. Obtener el texto de un elemento con la clase "list-group-item" cuando se hace clic sobre él
+ *    y mostrarlo en la consola o en un alert.
+ *
+ * 2. Cambiar de color el texto del elemento con id "itemNum2" al cargar el DOM por primera vez.
+ *
+ * 3. Poner en negrita y mayúscula el texto de los elementos con la clase "liClass" al cargar el DOM
+ *    por primera vez.
+ *
+ * 4. Adicionar la clase "alert-info" al elemento con la clase "alerta" y eliminar la clase "alert-danger"
+ *    cuando se cargue el DOM por primera vez.
+ *
+ * 5. Convertir el botón en un Toggle para mostrar u ocultar la alerta:
+ *    Por defecto la alerta se debe estar mostrando.
+ *    Al hacer click sobre el botón debe mostrar u ocultar la alerta.
+ *    El texto del botón debe cambiar a "Mostrar" u "Ocultar"
+ *
+ * 6. Adicionar elementos al inicio o final de la lista al hacer click en los botones "Inicio" o "Final".
+ *
+ * 7. En el punto anterior tener en cuenta lo que se escribe en la caja de texto y limpiar la caja.
+ *
+ * 8. Adicionar elementos antes y después de la lista desordenada.
+ */
---
+++ js/02-forms.js
@@ -0,0 +1,45 @@
+/**
+ * https://jquery.com/
+ * https://jqueryvalidation.org/validate/
+ * https://getbootstrap.com/docs/3.3/getting-started/
+ * https://jqueryui.com/
+ */
+$(document).ready(async () => {
+  // en esta sección se escribe el código que debe ejecutar cuando se cargue el DOM
+});
+// en esta sección se escribe el código que se dispara con eventos
+
+/******************************************
+ * Servicios disponibles
+ ******************************************
+ * Enviar datos por método GET
+ *
+ * Enviar datos por método POST
+ *
+ * Obtener departamentos
+ *
+ * Obtener ciudades
+ */
+
+/**
+ * Guía
+ *
+ * 1. Obtener los departamentos y ponerlos en el select de departamentos.
+ *
+ * 2. Adicionar el elemento "Seleccionar" por defecto y sin valor.
+ *
+ * 3. Al cambiar de departamento, debe consultar las ciudades correspondientes.
+ *
+ * 4. Al enviar los datos, prevenir que el formulario redireccione.
+ *
+ * 5. Asignar el método o tipo de petición que hace el formulario. (preventDefault)
+ *
+ * 6. Al presionar el botón GET, se deben enviar los datos al servidor con este método.
+ *
+ * 7. Al presionar el botón POST, se deben enviar los datos al servidor con este método.
+ *
+ * 8. Hacer funcionar el contador con los eventos de incrementar y decrementar.
+ *
+ * 9. Clonar el contador original y generar nuevos contadores independientes.
+ *
+ */
